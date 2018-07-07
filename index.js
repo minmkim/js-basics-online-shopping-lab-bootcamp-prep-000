944b9fdb77d89a7b744ff36f764b296bfe167474
Min Kim <minmkim@gmail.com>
2018-07-07 05:41:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -58,12 +58,12 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  if (cardNumber === null) {
+  if (cardNumber === "") {
     return "Sorry, we don't have a credit card on file for you."
   } else {
     var totalCost = total()
     cart.length = 0
-    return `Your total cost is $${totalCost}, which will be charged to the card ${cardNumber}`
+    return `Your total cost is $${totalCost}, which will be charged to the card ${cardNumber}.`
     
   }
 }
