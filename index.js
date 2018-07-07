2cf92e06bab37aacc01aed95ca2342ec05f98c90
Min Kim <minmkim@gmail.com>
2018-07-07 04:50:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  var itemDescription = { item, Math.random() }
+  cart.push(itemDescription)
 }
 
 function viewCart() {
